ForgetMult can't assume gradient passed to backwards is contiguous + FP16 convert bug with Nones

torchqrnn/forget_mult.py
@@ -115,8 +115,8 @@ class GPUForgetMult(torch.autograd.Function):
 
     def forward(self, f, x, hidden_init=None):
         self.compile()
-        assert f.is_contiguous() and x.is_contiguous(), 'Both inputs must be contiguous for ForgetMult GPU kernel'
-        seq_size, batch_size, hidden_size = f.size()
+        assert f.is_contiguous() and x.is_contiguous(), 'Inputs must be contiguous for ForgetMult GPU kernel'
+        seq_size, batch_size, hidden_size = x.size()
         result = f.new(seq_size + 1, batch_size, hidden_size)
         # We only zero the result array (result[0]) if we don't set a hidden initial state
         # All other values (result[1:]) are overwritten by default
@@ -133,9 +133,11 @@ class GPUForgetMult(torch.autograd.Function):
     def backward(self, grad_h):
         self.compile()
         f, x, hidden_init = self.saved_tensors
+        # There is no guarantee the gradient tensor is contiguous
+        grad_h = grad_h.contiguous()
         h = self.result
         ###
-        seq_size, batch_size, hidden_size = f.size()
+        seq_size, batch_size, hidden_size = x.size()
         # Zeroing is not necessary as these will be overwritten
         grad_f = f.new(*f.size())
         grad_x = f.new(*f.size())

torchqrnn/qrnn.py
@@ -9,7 +9,6 @@ else:
 
 
 class LayerNorm(nn.Module):
-
     def __init__(self, features, eps=1e-6):
         super().__init__()
         self.gamma = nn.Parameter(torch.ones(features))
@@ -114,7 +113,7 @@ class QRNNLayer(nn.Module):
         # Forget Mult
         # For testing QRNN without ForgetMult CUDA kernel, C = Z * F may be useful
         # Convert to and from float in the case the QRNN ForgetMult is being used with FP16
-        F, Z, hidden = (v.float() for v in (F, Z, hidden))
+        F, Z, hidden = (v.float() if v is not None else v for v in (F, Z, hidden))
         C = ForgetMult()(F, Z, hidden, use_cuda=self.use_cuda)
         C = C.type_as(source)
 